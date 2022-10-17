6822930ef26e13e254e2c853f19dc4f466b515f3
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2022-10-17 11:16:45 +0100

    Minimise application to tray

--- main.js
+++ main.js
@@ -1,25 +1,53 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Tray, Menu } = require("electron");
+
+let isQuitting = false;
+let tray;
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-        autoHideMenuBar: true
-    });
+	const win = new BrowserWindow({
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+		autoHideMenuBar: true,
+		icon: "icon.png"
+	});
   
-    win.loadFile("index.html");
+	win.loadFile("index.html");
+
+	tray = new Tray("icon.png");
+
+	tray.setContextMenu(Menu.buildFromTemplate([
+		{
+			label: "Open", click: () => {
+				win.show();
+			}
+		},
+		{
+			label: "Quit", click: () => {
+				isQuitting = true;
+				app.quit();
+			}
+		}
+	]));
+
+	win.on("close", (event) => {
+		if (!isQuitting) {
+			event.preventDefault();
+			win.hide();
+			event.returnValue = false;
+		}
+	});
 }
 
 app.whenReady().then(() => {
-    createWindow();
+	createWindow();
   
-    app.on("activate", () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    });
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	});
 });
 
-app.on("window-all-closed", () => {
-    if (process.platform !== 'darwin') app.quit()
+app.on("before-quit", () => {
+	isQuitting = true;
 });
