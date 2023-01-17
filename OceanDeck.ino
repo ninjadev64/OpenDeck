736816ebf96d5cdc47b516fee0743f1168c02389
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2023-01-17 17:30:08 +0000

    Update OceanDeck.ino

--- OceanDeck.ino
+++ OceanDeck.ino
@@ -1,37 +1,25 @@
 #include "ArduinoJson.h";
 
 int lastPressed;
-char incomingMessage[64];
-int incomingMessagePos = 0;
 
 void setup() {
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+
   Serial.begin(57600);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char inByte = Serial.read();
-    if (inByte != '\n') {
-      incomingMessage[incomingMessagePos] = inByte;
-      incomingMessagePos++;
-    } else {
-      StaticJsonDocument<64> doc;
-      deserializeJson(doc, incomingMessage);
-      analogWrite(9, doc["red"].as<uint8_t>());
-      analogWrite(10, doc["green"].as<uint8_t>());
-      analogWrite(11, doc["blue"].as<uint8_t>());
-      incomingMessagePos = 0;
-      memset(incomingMessage, 0, sizeof(incomingMessage));
-    }
-  }
-  
   StaticJsonDocument<16> doc;
-  /**/ if (digitalRead(2) == HIGH) doc["button"] = 1;
-  else if (digitalRead(3) == HIGH) doc["button"] = 2;
-  else if (digitalRead(4) == HIGH) doc["button"] = 3;
-  else {
-    doc["button"] = 0;
-  }
+  /**/ if (digitalRead(2) == LOW) doc["button"] = 1;
+  else if (digitalRead(3) == LOW) doc["button"] = 2;
+  else if (digitalRead(4) == LOW) doc["button"] = 3;
+  else doc["button"] = 0;
   
   if (lastPressed == doc["button"]) return;
   lastPressed = doc["button"];
