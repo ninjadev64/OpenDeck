4e7be5f1d95ce1349cd3bf429f53f02a1d0d004b
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2023-11-18 17:12:18 +0000

    Implement communication with ProntoKey devices

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,13 @@
 // Prevents additional console window on Windows in release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod devices;
+
 fn main() {
-	tauri::Builder::default()
-		.run(tauri::generate_context!())
-		.expect("error while running tauri application");
+	devices::initialise_devices();
+
+	match tauri::Builder::default().run(tauri::generate_context!()) {
+		Err(e) => panic!("Error while running Tauri application: {}", e.to_string()),
+		_ => {}
+	}
 }
