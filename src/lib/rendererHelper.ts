9897dbac7fd7afc8599bd21fc7954b07b6f16e62
OmegaRogue <omegarogue@omegavoid.codes>
2024-02-23 18:06:53 +0100

    Add slashes before image paths

--- src/lib/rendererHelper.ts
+++ src/lib/rendererHelper.ts
@@ -59,7 +59,7 @@ export async function renderImage(actionContext: string, state: ActionState, sho
 	if (showOk) {
 		let okImage = document.createElement("img");
 		okImage.crossOrigin = "anonymous";
-		okImage.src = "ok.png";
+		okImage.src = "/ok.png";
 		await new Promise((resolve) => {
 			okImage.onload = resolve;
 		});
@@ -69,7 +69,7 @@ export async function renderImage(actionContext: string, state: ActionState, sho
 	if (showAlert) {
 		let alertImage = document.createElement("img");
 		alertImage.crossOrigin = "anonymous";
-		alertImage.src = "alert.png";
+		alertImage.src = "/alert.png";
 		await new Promise((resolve) => {
 			alertImage.onload = resolve;
 		});
