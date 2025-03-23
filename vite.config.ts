8832870fd166301d0268549d7f57eb2c780fda09
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2025-03-23 14:52:58 +0000

    chore: integrate tailwindcss with vite and remove postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
+import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 	clearScreen: false,
 	server: {
 		watch: {
