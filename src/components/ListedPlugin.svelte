3d31a213c1f9339011316282441e0e135aca177d
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2025-04-10 22:17:10 +0200

    perf: load plugin manager icons lazily

--- src/components/ListedPlugin.svelte
+++ src/components/ListedPlugin.svelte
@@ -10,7 +10,7 @@
 	class="flex flex-row items-center m-2 p-2 bg-neutral-200 dark:bg-neutral-700 rounded-md"
 	class:hidden
 >
-	<img src={icon} class="w-24 h-24 rounded-md" alt={name} />
+	<img src={icon} class="w-24 h-24 rounded-md" alt={name} loading="lazy" />
 	<div class="ml-4 mr-2 dark:text-neutral-300 [overflow-wrap:anywhere]">
 		<p class="font-semibold">{name}</p> {subtitle}
 	</div>
